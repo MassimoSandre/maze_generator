50d4675f08ecf3e378794686e097ccf2aa9584c9
Massimo Sandretti <massimo.sandretti@gmail.com>
2022-02-13 03:10:54 +0100

    made a maze solver class

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 import random
 from maze import Maze
+from maze_solver import Maze_Solver
 
 size = width, height = 800, 800
 
@@ -14,15 +15,20 @@ clock = pygame.time.Clock()
 
 pygame.font.init()
 
+font = pygame.font.SysFont('arial', 20)
+
 running = True
 
-MAZE_SIZE = (20,20)
+MAZE_SIZE = (10,10)
 CELL_SIZE = 40
 
 m = Maze(MAZE_SIZE)
-
+solver = Maze_Solver(m)
+sol = solver.get_solution()
 
 bx, by  = (100,100)
+
+print(sol)
 
 while running:
     screen.fill(bgcolor)
@@ -32,17 +38,30 @@ while running:
         for y in range(MAZE_SIZE[1]):
             if not (x-1, y, x, y) in m.doors and not (x+y==0):
                 pygame.draw.line(screen, white, (bx+x*CELL_SIZE, by+y*CELL_SIZE),(bx+x*CELL_SIZE, by+(y+1)*CELL_SIZE))   
+            else:
+                if (x-1, y) in sol and (x,y) in sol:
+                    pygame.draw.line(screen, (0,255,0), (bx+x*CELL_SIZE - CELL_SIZE//2, by+y*CELL_SIZE + CELL_SIZE//2),(bx+x*CELL_SIZE + CELL_SIZE//2,by+y*CELL_SIZE + CELL_SIZE//2))   
 
             if not (x,y,x+1,y) in m.doors:
                 pygame.draw.line(screen, white, (bx+(x+1)*CELL_SIZE, by+y*CELL_SIZE),(bx+(x+1)*CELL_SIZE, by+(y+1)*CELL_SIZE))   
+            else:
+                if (x, y) in sol and (x+1,y) in sol:
+                    pygame.draw.line(screen, (0,255,0), (bx+x*CELL_SIZE + CELL_SIZE//2, by+y*CELL_SIZE + CELL_SIZE//2),(bx+x*CELL_SIZE + 3*(CELL_SIZE//2),by+y*CELL_SIZE + CELL_SIZE//2))   
                 
             if not (x,y-1,x,y) in m.doors:
                 pygame.draw.line(screen, white, (bx+(x)*CELL_SIZE, by+y*CELL_SIZE),(bx+(x+1)*CELL_SIZE, by+(y)*CELL_SIZE))   
+            else:
+                if (x, y-1) in sol and (x,y) in sol:
+                    pygame.draw.line(screen, (0,255,0), (bx+x*CELL_SIZE + CELL_SIZE//2, by+y*CELL_SIZE - CELL_SIZE//2),(bx+x*CELL_SIZE + CELL_SIZE//2, by+y*CELL_SIZE + CELL_SIZE//2))   
                 
-            
             if not (x,y,x,y+1) in m.doors and not (x+y==MAZE_SIZE[0]+MAZE_SIZE[1]-2):
                 pygame.draw.line(screen, white, (bx+(x)*CELL_SIZE, by+(y+1)*CELL_SIZE),(bx+(x+1)*CELL_SIZE, by+(y+1)*CELL_SIZE))   
-                
+            else:
+                if (x, y) in sol and (x,y+1) in sol:
+                    pygame.draw.line(screen, (0,255,0), (bx+x*CELL_SIZE + CELL_SIZE//2, by+y*CELL_SIZE + CELL_SIZE//2),(bx+x*CELL_SIZE + CELL_SIZE//2, by+y*CELL_SIZE + 3*(CELL_SIZE//2)))   
+
+            
+
 
     pygame.display.update()
     clock.tick(60)
